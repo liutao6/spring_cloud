c01ffa34f0e1678872f6c3c2a0f439ebdf23c6c7
刘涛 <tao.liu@yopark.com>
2017-08-14 17:27:39 +0800

    test

--- hb-user/src/main/java/com/liutao/user/base/config/SwaggerConfig.java
+++ hb-user/src/main/java/com/liutao/user/base/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.liutao.user.base.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -20,6 +21,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 @ConditionalOnProperty(prefix = "spirngcloud", name = "swagger-open", havingValue = "true")
+//@Profile({"dev", "test"})//在生产环境不开启 也可以使用上面的配置
 public class SwaggerConfig {
 
     @Bean
